550fa2bde2b01f6d79f1af261cd325476f7d69ef
Cschmi22 <119636252+Cschmi22@users.noreply.github.com>
2023-02-02 13:09:57 -0500

    Add files via upload

---
+++ Variables_Exercise2/Variables_Exercise2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+string name = "Caleb";
+int age = 27;
+char middleInitial = 'T';
+bool isOver18 = true;
+double currentTemp = 45.2;
+decimal currentPrice = 29.99m;
+
+
+Console.WriteLine($"Name: {name}\n" +
+                  $"Age: {age}\n" +
+                  $"middleInitial: {middleInitial}\n" +
+                  $"isOver18: {isOver18}\n" +
+                  $"currentTemp: {currentTemp}\n" +
+                  $"currentPrice: {currentPrice}\n");
